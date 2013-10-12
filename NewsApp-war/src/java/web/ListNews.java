0cf5b4c0b9653684264ae92262ed57667d4348ae
albf <alexandre@Alexandres-MacBook-Air.local>
2013-10-12 22:12:47 +0200

    NewsApp files

---
+++ NewsApp-war/src/java/web/ListNews.java
@@ -0,0 +1,121 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+import ejb.NewsEntity;
+import ejb.NewsEntityFacade;
+import ejb.SessionManagerBean;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author alexandre
+ */
+@WebServlet(name = "ListNews", urlPatterns = {"/ListNews"})
+public class ListNews extends HttpServlet {
+    @EJB
+    private SessionManagerBean sessionManagerBean;
+    @EJB
+    private NewsEntityFacade newsEntityFacade;
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getSession(true);
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ListNews</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ListNews at " + request.getContextPath() + "</h1>");
+            
+            List news = newsEntityFacade.findAll();
+            //List news = newsEntityFacade.find(1);
+            //news.add (newsEntityFacade.find(1));
+            
+            for (Iterator it = news.iterator(); it.hasNext();) {
+                //NewsEntity elem = (NewsEntity) it.next();
+                long n=1;
+                NewsEntity elem = newsEntityFacade.find(n);
+                
+                out.println(" <b>"+"Id: "+elem.getId()+" </b><br />");
+                out.println(" <b>"+elem.getTitle()+" </b><br />");
+                out.println(elem.getBody()+"<br /> ");
+            }
+            out.println("<a href='PostMessage'>Add new message</a>");
+            
+            out.println("<br><br>");
+            out.println(sessionManagerBean.getActiveSessionsCount() + " user(s) reading the news.");
+            
+            out.println("</body>");
+            out.println("</html>");
+        } finally {            
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
